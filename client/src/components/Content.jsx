8ed88591710ac258a8560e9bb89e533249ca1ced
Yazid0Hakimi <71094777+Yazid0Hakimi@users.noreply.github.com>
2023-04-06 23:08:00 +0000

    .env file

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -3,8 +3,10 @@ import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import { TextField } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import {Button} from "@mui/material";
+import { Button } from "@mui/material";
 import { useState } from "react";
+
+
 
 const theme = createTheme({
   palette: {
@@ -17,19 +19,33 @@ const theme = createTheme({
 function Content() {
   const [textFieldValue, setTextFieldValue] = useState("");
 
-const sendDataToServer = async () => {
-  const data = {data : textFieldValue};
+  const sendDataToServer = async ( inputValue) => {
+    const artist = inputValue; ;
+    const accessToken =
+      "BQDqz3yYJYKTnXYcXcR7glGp9sglDKeKMZNy0jfq9jGTYWvd13t1wSvC0s1CSAyTmtZVvG41YLR1SjJY8Ell5zvM4vP6GhZlGBDzPo4C51zwnsRRTic5";
 
-  // console.log(JSON.stringify(data));
- const gg = await fetch("http://localhost:5000/post", {
-   method: "POST",
-   headers: {
-     "Content-Type": "application/json",
-   },
-   body: JSON.stringify(data) ,
- });
-return gg.json();
-}
+    fetch(`https://api.spotify.com/v1/search?q=${artist}&type=track`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // Display the response data on the page
+        const tracks = data.tracks.items;
+        const trackList = document.createElement("ul");
+        tracks.forEach((track) => {
+          const trackItem = document.createElement("li");
+          trackItem.innerText = track.name;
+          trackList.appendChild(trackItem);
+        });
+        document.body.appendChild(trackList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <Box flex={10}>
@@ -61,7 +77,9 @@ return gg.json();
               left: "38%",
               color: "white",
             }}
-            onClick={ ()=>{sendDataToServer().then((data) => console.log(data))}}
+            onClick={() => {
+              sendDataToServer().then((data) => console.log(data));
+            }}
           >
             submit
           </Button>
--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
-const bodyParser = require("body-parser");
+require("dotenv").config();
 
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cors());
-
 app.use(express.json()); // for parsing application/json
 
 const PORT = process.env.PORT || 5000;
-
-let gross = [{ item1: "1" }, { item2: "2" }, { item2: "3" }];
+const dbUrl =
+  process.env.access_token ||
+  "BQDqz3yYJYKTnXYcXcR7glGp9sglDKeKMZNy0jfq9jGTYWvd13t1wSvC0s1CSAyTmtZVvG41YLR1SjJY8Ell5zvM4vP6GhZlGBDzPo4C51zwnsRRTic5";
 
 app.get("/get", (req, res) => {
   res.send(gross);
